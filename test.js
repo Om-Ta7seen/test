eea8bfacef1a519e6110cfdfce3e6531657ee35d
Zainab Hammami <zainab.hammami.zh@gmail.com>
2017-02-12 17:09:39 +0200

    test

---
+++ test.js
@@ -0,0 +1,5 @@
+1- If you make a change in any file you should make the steps we used to know
+like git add, git commit, and then push it into you'r repo.
+2- When you finish pushing you'r code, make a pull request, and after I merge it,
+make sure that all the files you have on you'r local are the same of the ones 
+on the github using this command: git checkout master.
